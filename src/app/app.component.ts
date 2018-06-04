8501cf0b858ac74ab7df66c74c77d760bb777923
achi-dev <achirawat@angstromsolutions.net>
2018-06-04 14:49:03 +0700

    first pdfmake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello'
+
+  constructor(){
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    let docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+    pdfMake.createPdf(docDefinition).open();
+  }
 }
